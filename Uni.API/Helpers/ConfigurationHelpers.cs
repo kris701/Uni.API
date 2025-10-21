f43799f295b8c665f0a3593319dba0872a82120c
Kristian Skov <kris701kj@gmail.com>
2025-10-21 08:19:26 +0200

    Helper fix

--- Uni.API/Helpers/ConfigurationHelpers.cs
+++ Uni.API/Helpers/ConfigurationHelpers.cs
@@ -26,9 +26,15 @@ namespace Uni.API.Helpers
 		/// <exception cref="Exception"></exception>
 		public static T GetSectionValue<T>(this IConfiguration configuration, string section, string key)
 		{
-			var value = configuration.GetSection(section).GetSection(key).Get<T>();
-			if (value == null)
+			if (!configuration.DoesSectionExist(section))
+				throw new Exception($"Section '{section}' was not set in IConfiguration!");
+			var valueSection = configuration.GetSection(section).GetSection(key);
+			if (!valueSection.Exists())
 				throw new Exception($"'{key}' for section '{section}' was not set in IConfiguration!");
+
+			var value = valueSection.Get<T>();
+			if (value == null)
+				throw new Exception($"The key value was null for the key '{key}' and section '{section}' in IConfiguration!");
 			return value;
 		}
 	}
