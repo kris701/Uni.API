06535366be47f51964e6cecbcc23b208db0c24fa
Kristian Skov <kris701kj@gmail.com>
2026-06-04 12:23:19 +0200

    Changed to webapplication builder

--- Uni.API/UniAPIBuilder.cs
+++ Uni.API/UniAPIBuilder.cs
@@ -1,31 +1,29 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Uni.API
 {
 	/// <summary>
-	/// A default <seealso cref="IHostBuilder"/> for the Uni.API
+	/// A default <seealso cref="WebApplicationBuilder"/> for the Uni.API
 	/// </summary>
 	public static class UniAPIBuilder
 	{
 		/// <summary>
-		/// Create a new instance of a <seealso cref="IHostBuilder"/> based on the startup <typeparamref name="T"/>
+		/// Create a new instance of a <seealso cref="WebApplicationBuilder"/> based on the startup <typeparamref name="T"/>
 		/// This also loads a json config file with the nae 'configuration.json', if it exists.
 		/// </summary>
 		/// <typeparam name="T"></typeparam>
 		/// <param name="args"></param>
 		/// <param name="configFile"></param>
 		/// <returns></returns>
-		public static IHostBuilder CreateUniAPIBuilder<T>(string[] args, string configFile = "configuration.json") where T : UniAPIStartup
+		public static WebApplicationBuilder CreateUniAPIBuilder<T>(string[] args, string configFile = "configuration.json") where T : UniAPIStartup
 		{
-			var builder = Host.CreateDefaultBuilder(args);
-			builder.ConfigureAppConfiguration((context, config) =>
-			{
-				if (File.Exists(configFile))
-					config.AddJsonFile(configFile);
-			});
-			builder.ConfigureWebHostDefaults(webBuilder =>
+			var builder = WebApplication.CreateBuilder(args);
+			if (File.Exists(configFile))
+				builder.Configuration.AddJsonFile(configFile);
+			builder.Host.ConfigureWebHostDefaults(webBuilder =>
 			{
 				webBuilder.UseStartup<T>();
 			});
